d516c77af2dcb902edc5c2678baa079117f4592d
Thamsanqa <thamue1892@gmail.com>
2023-05-08 23:38:31 +0200

    Handled Quering Getting an Unexisting Company

---
+++ Entities/Exceptions/CompanyNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Exceptions
+{
+    public sealed class CompanyNotFoundException:Exception
+    {
+        public CompanyNotFoundException(Guid companyId)
+        :base($"Company with id: {companyId} doesn't exist in the database.")
+        {
+            
+        }
+    }
+}
--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
 using Entities.Models;
 using Service.Contracts;
 using Shared.DataTransferObjects;
@@ -32,9 +33,12 @@ namespace Service
             
         }
 
-        public CompanyDto GetCompany(Guid companyId, bool trackChanges)
+        public CompanyDto GetCompany(Guid id, bool trackChanges)
         {
-            var company = _repository.Company.GetCompany(companyId, trackChanges);
+            var company = _repository.Company.GetCompany(id, trackChanges);
+            if (company == null)
+                throw new CompanyNotFoundException(id);
+
                 var companyDto = _mapper.Map<CompanyDto>(company);
                 return companyDto;
         }

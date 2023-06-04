4964a163922704e5818180549ced4b5c6cc9a04a
Thamsanqa <thamue1892@gmail.com>
2023-06-04 23:12:55 +0200

    Implemented UPDATE Company with employees

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -95,5 +95,25 @@ namespace Service
             _repository.Company.DeleteCompany(company);
             _repository.Save();
         }
+
+        public void DeleteCompany(Guid companyId,CompanyForUpdateDto companyForUpdateDto,bool trackChanges)
+        {
+            var companyEntity = _repository.Company.GetCompany(companyId, trackChanges);
+            if(companyEntity is null)
+                throw new CompanyNotFoundException(companyId);
+
+            _mapper.Map(companyForUpdateDto, companyEntity);
+            _repository.Save();
+        }
+
+        public void UpdateCompany(Guid companyId, CompanyForUpdateDto companyForUpdate, bool trackChanges)
+        {
+            var companyEntity = _repository.Company.GetCompany(companyId, trackChanges);
+            if (companyEntity is null)
+                throw new CompanyNotFoundException(companyId);
+
+            _mapper.Map(companyForUpdate, companyEntity);
+            _repository.Save();
+        }
     }
 }

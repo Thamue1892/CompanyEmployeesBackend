892267f8a85c299ee5be96687396223f1e33c3c3
Thamsanqa <thamue1892@gmail.com>
2023-06-07 21:48:56 +0200

    Implemented Validation for PATCH Requests

--- CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -82,7 +82,16 @@ namespace CompanyEmployees.Presentation.Controllers
                 return BadRequest("patchDoc object sent from client is null");
 
            var result = _service.EmployeeService.GetEmployeeForPatch(companyId, id, compTrackChanges: false, empTrackChanges: true);
-           patchDoc.ApplyTo(result.employeeToPatch);
+           patchDoc.ApplyTo(result.employeeToPatch,ModelState);
+           TryValidateModel(result.employeeToPatch);
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+          
+          
+            _service.EmployeeService.SaveChangesForPatch(result.employeeToPatch, result.employeeEntity);
+            return NoContent();
 
            _service.EmployeeService.SaveChangesForPatch(result.employeeToPatch, result.employeeEntity);
             return NoContent();

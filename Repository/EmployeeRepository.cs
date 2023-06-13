25307edf5c63dd58eb5c0d170da94527088b6fa6
Thamsanqa <thamue1892@gmail.com>
2023-06-13 22:03:13 +0200

    Implemented Sorting

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -19,6 +19,7 @@ internal sealed class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRe
         var employees = await FindByCondition(e=>e.CompanyId.Equals(companyId),trackChanges)
             .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
             .Search(employeeParameters.SearchTerm)
+            .Sort(employeeParameters.OrderBy)
             .OrderBy(e => e.Name)
             .ToListAsync();
 

f751bcae8e31e7b2946d582b435f3b82576844c1
Thamsanqa <thamue1892@gmail.com>
2023-06-19 01:31:25 +0200

    Implemented Authentication

--- Service/AuthenticationService.cs
+++ Service/AuthenticationService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -8,37 +10,100 @@ using Contracts;
 using Entities.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Service.Contracts;
 using Shared.DataTransferObjects;
 
 namespace Service
 {
-    internal sealed class AuthenticationService:IAuthenticationService
+    internal sealed class AuthenticationService : IAuthenticationService
     {
-       private readonly ILoggerManager _logger;
-       private readonly UserManager<User> _userManager;
-       private readonly IMapper _mapper;
-       private readonly IConfiguration _configuration;
+        private readonly ILoggerManager _logger;
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
 
-       public AuthenticationService(ILoggerManager logger,IMapper mapper,UserManager<User> userManager,IConfiguration configuration)
-       {
-           _logger = logger;
-           _mapper = mapper;
-           _userManager = userManager;
-           _configuration = configuration;
+        private User? _user;
 
-       }
+        public AuthenticationService(ILoggerManager logger, IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userManager = userManager;
+            _configuration = configuration;
 
-       public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration)
-       {
-           var user = _mapper.Map<User>(userForRegistration);
-           var result = await _userManager.CreateAsync(user, userForRegistration.Password);
-           if (result.Succeeded)
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration)
+        {
+            var user = _mapper.Map<User>(userForRegistration);
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
+            }
+
+            return result;
+        }
+
+        public async Task<bool> ValidateUser(UserForAuthenticationDto userForAuth)
+        {
+            _user = await _userManager.FindByNameAsync(userForAuth.UserName);
+
+            var result = (_user != null && await _userManager.CheckPasswordAsync(_user, userForAuth.Password));
+            if (!result)
+                _logger.LogWarn($"{nameof(ValidateUser)}: Authentication failed. Wrong username or password.");
+
+            return result;
+        }
+
+        public async Task<string> CreateToken()
+        {
+            var signingCredentials = GetSigningCredentials();
+            var claims = await GetClaims();
+            var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SECRET"));
+            var secret = new SymmetricSecurityKey(key);
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim>
            {
-               await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
-           }
+               new Claim(ClaimTypes.Name, _user.UserName)
+           };
 
-           return result;
-       }
+            var roles = await _userManager.GetRolesAsync(_user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+            var tokenOptions = new JwtSecurityToken
+            (
+                issuer: jwtSettings["validIssuer"],
+                audience: jwtSettings["validAudience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["expires"])),
+                signingCredentials: signingCredentials
+            );
+
+            return tokenOptions;
+        }
     }
 }
